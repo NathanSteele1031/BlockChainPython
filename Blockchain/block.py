f6b0fa6b37ed37774749a762f73b15d332249d51
NathanSteele1031 <nsteele1031@gmail.com>
2026-06-15 20:31:26 -0400

    Added base classes and made block class.

---
+++ Blockchain/block.py
@@ -0,0 +1,19 @@
+import hashlib
+
+class Block:
+    def __init__(self, infomation: str, previous_hash):
+        self.previous_hash = previous_hash
+        self.info = infomation
+        self.key = None
+        self.hash = None
+
+    def test_key(self, given_key: int):
+        h = hashlib.new("sha256")
+        h.update(str(self.previous_hash).encode())
+        h.update(self.info.encode())
+        h.update(str(given_key).encode())
+        if h.hexdigest()[-6:] == "000000":
+            self.key = given_key
+            self.hash = h.hexdigest()
+            return True
+        return False
