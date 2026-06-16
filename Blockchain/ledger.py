f86f3c0d47d8a7d1564bd0f222337a0fa3ea8d0e
NathanSteele1031 <nsteele1031@gmail.com>
2026-06-15 20:50:13 -0400

    Added the solve method

--- Blockchain/ledger.py
+++ Blockchain/ledger.py
@@ -8,11 +8,16 @@ class Ledger:
     def add_block(self, infomation: str):
         if self.length == 0:
             self.blocks.append(block.Block(infomation, None))
+            self.length += 1
         else:
             self.blocks.append(block.Block(infomation, self.blocks[-1].hash))
+            self.length += 1
     
     def solve(self):
-        pass
+        solve_key = 0
+        while not self.blocks[-1].test_key(solve_key):
+            solve_key += 1
+        print("Sovled")
 
     def show(self):
         for selected_block in self.blocks:
