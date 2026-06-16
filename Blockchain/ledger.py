6910ea6622a3490d17767bd0dc2655d050d0f328
NathanSteele1031 <nsteele1031@gmail.com>
2026-06-15 20:36:30 -0400

    Added the baseline methods and worked on add_block

--- Blockchain/ledger.py
+++ Blockchain/ledger.py
@@ -1,3 +1,18 @@
+from Blockchain.block import Block
+
 class Ledger:
     def __init__(self):
+        self.blocks = []
+        self.length = 0
+
+    def add_block(self, infomation: str):
+        if self.length == 0:
+            self.blocks.append(Block(infomation, None))
+        else:
+            self.blocks.append(Block(infomation, self.blocks[-1].hash))
+    
+    def solve(self):
+        pass
+
+    def show(self):
         pass
