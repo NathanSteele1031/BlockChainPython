33662f7487a5db938c6b03691642a117256d3787
Nathan Steele <nsteele1031@gmail.com>
2026-06-18 08:34:32 -0400

    Added menu for mining or creating blocks.

---
+++ main.py
@@ -0,0 +1,20 @@
+from Blockchain.block import Block
+from Blockchain.ledger import Ledger
+
+def main():
+    print("Loading ledger")
+    ledger = Ledger()
+    # Load the whole or part of the ledger here
+    print("1.Mine on the blockchain\n2.Create a block")
+    user_input = input(": ")
+    if user_input == "1":
+        # confirm the chain is up to date and start listening for new blocks and work on them.
+        pass
+
+    if user_input == "2":
+        block_info = input("Type the data that will go into the blockchain: ")
+        ledger.add_block(block_info)
+        # Broadcast block through p2p connections
+
+if __name__ == "__main__":
+    main()
